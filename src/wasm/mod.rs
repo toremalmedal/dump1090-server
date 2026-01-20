703dd12f4a7933cc07c5365a0fb6e411dff0ac37
Tore Malmedal <t.malmedal@gmail.com>
2026-01-20 11:26:50 +0100

    fix: adds wasm client

--- src/wasm/mod.rs
+++ src/wasm/mod.rs
@@ -1,5 +1,5 @@
 use super::proto::flight_service_client::FlightServiceClient;
-use super::proto::{Empty, FlightData, Receiver};
+use super::proto::{Empty, FlightData, HistoricalData, Receiver};
 
 use std::error::Error;
 use tonic_web_wasm_client::Client;
@@ -24,5 +24,16 @@ pub async fn get_receiver_data(
     let req = Empty {};
     let request = tonic::Request::new(req);
     let response = client.get_receiver_data(request).await?;
+    Ok(response)
+}
+
+pub async fn get_historical_data(
+    server_url: String,
+) -> Result<tonic::Response<HistoricalData>, Box<dyn Error>> {
+    let wasm_client = Client::new(server_url);
+    let mut client = FlightServiceClient::new(wasm_client);
+    let req = Empty {};
+    let request = tonic::Request::new(req);
+    let response = client.get_historical_data(request).await?;
     Ok(response)
 }
